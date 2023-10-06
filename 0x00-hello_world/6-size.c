b8d7c091b00067110ef654aedabc7032443d6f24
koffsinmax <ezekielappiah8@gmail.com>
2023-10-05 20:00:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 
 	printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of int: %1u byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %1u bye(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
