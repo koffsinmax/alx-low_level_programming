ab2dc36fb460c2fa8893dd9bf443989d1c3afbc9
koffsinmax <ezekielappiah8@gmail.com>
2023-10-05 18:42:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints exactly with proper grammar
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
